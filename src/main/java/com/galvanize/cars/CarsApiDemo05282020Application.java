08d53f22458d3e5b9d497493fd8f40be9bb47e9a
rlwing <rob@wingjr.com>
2020-05-28 16:30:23 -0700

    spring init

---
+++ src/main/java/com/galvanize/cars/CarsApiDemo05282020Application.java
@@ -0,0 +1,13 @@
+package com.galvanize.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApiDemo05282020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsApiDemo05282020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/cars/CarsApiDemo05282020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApiDemo05282020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
