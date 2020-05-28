ceb58dd84a1a9d19e0294f3b4cdf79b1b69c77eb
rlwing <rob@wingjr.com>
2020-05-28 16:45:46 -0700

    Step 1

--- src/test/java/com/galvanize/cars/CarsApiDemo05282020ApplicationTests.java
+++ src/test/java/com/galvanize/cars/CarsApiDemo05282020ApplicationTests.java
@@ -1,13 +1,27 @@
 package com.galvanize.cars;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CarsApiDemo05282020ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	TestRestTemplate restTemplate;
 
+	@Test
+	void getCarDetails_shouldReturnCar() {
+		ResponseEntity<Car> response = restTemplate.getForEntity("/cars/prius", Car.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().getName()).isEqualTo("prius");
+		assertThat(response.getBody().getType()).isEqualTo("hybrid");
+	}
 }
